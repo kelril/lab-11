6bc1b0eb6982e27f097cd52e4e40aa9afcc0483c
kelril <kelril@dsu1.service.sarnet>
2020-05-22 11:14:49 +0300

    0.2.5

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,7 +1,7 @@
 #ifndef UNTITLED_CONSTANTS_H
 #define UNTITLED_CONSTANTS_H
-const std::string command_1 = "cmake -H. -B_build -DCMAKE_INSTALL_" + std::string("PREFIX=_install -DCMAKE_BUILD_TYPE=");
-const std::string command_2 = "cmake --build _build";
-const std::string command_3 = "cmake --build _build --target install";
-const std::string command_4 = "cmake --build _build --target package";
+std::string command_1 = "cmake -H. -B_build -DCMAKE_INSTALL_" + std::string("PREFIX=_install -DCMAKE_BUILD_TYPE=");
+std::string command_2 = "cmake --build _build";
+std::string command_3 = "cmake --build _build --target install";
+std::string command_4 = "cmake --build _build --target package";
 #endif //UNTITLED_CONSTANTS_H
